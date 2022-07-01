300088789cb2f61d53d7451d13d1369c72007050
Hrishabh.Predator <hrishabhdivya246@gmail.com>
2022-07-01 13:19:51 +0530

    added InputText

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as sg  # import PySimpleGui
 # print(sg)#Testing if module has been loaded in my Virtual environment
 layout = [[sg.Text("Hello this is a practice for GUI",key='-OUT-'),
-           sg.Button("OKBye")], [sg.Button("Change")]]
+           sg.Button("OK")],[sg.InputText()], [sg.Button("Change")]]
 window = sg.Window("calculator", layout, margins=(100, 50))
 while True:
     event, value = window.read()
