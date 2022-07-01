034162157249e2e198d6b5964c97fb37b5d5c98c
Hrishabh.Predator <hrishabhdivya246@gmail.com>
2022-07-01 08:49:28 +0530

    Created First GUI

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg#import PySimpleGui
 # print(sg)#Testing if module has been loaded in my Virtual environment
-layout = [ [sg.Text("Hello"), sg.Button("OK")],[sg.Button("Cancel")]]
+layout = [ [sg.Text("Hello this is a practice for GUI"), sg.Button("OK")],[sg.Button("Cancel")]]
 window =sg.Window("calculator", layout)
 event,value = window.read()
 # print(event,value)
