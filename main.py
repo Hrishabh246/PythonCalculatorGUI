f6a2bb1d0b76fcfd2af7a0a0f5814d8b95ec8814
Hrishabh.Predator <hrishabhdivya246@gmail.com>
2022-07-01 12:22:37 +0530

    Cal0.1

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import PySimpleGUI as sg#import PySimpleGui
+import PySimpleGUI as sg  # import PySimpleGui
 # print(sg)#Testing if module has been loaded in my Virtual environment
-layout = [ [sg.Text("Hello this is a practice for GUI"), sg.Button("OK")],[sg.Button("Cancel")]]
-window =sg.Window("calculator", layout)
-event,value = window.read()
-# print(event,value)
-window.close()
-#editing file from Duo and now predator
+layout = [[sg.Text("Hello this is a practice for GUI",key='-OUT-'),
+           sg.Button("OKBye")], [sg.Button("Change")]]
+window = sg.Window("calculator", layout, margins=(100, 50))
+while True:
+    event, value = window.read()
+    print(event,value)
+    if event == sg.WIN_CLOSED:
+        break
+    if event == "Change":
+        window['-OUT-'].update(f'you clicked  {event}')
+        print(type(event))
+    
 
+
+window.close()
+# editing file from Duo and now predator
