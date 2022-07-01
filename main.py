e1db1f3062c571910c07d8bf339af7977c3df8c2
Hrishabh.Predator <hrishabhdivya246@gmail.com>
2022-07-01 15:14:47 +0530

    Adding Numbers

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import PySimpleGUI as sg  # import PySimpleGui
 # print(sg)#Testing if module has been loaded in my Virtual environment
-layout = [[sg.Text("Hello this is a practice for GUI",key='-OUT-'),
-           sg.Button("OKBye")], [sg.Button("Change")]]
-window = sg.Window("calculator", layout, margins=(100, 50))
+# all My functions are here...
+
+
+def add(num1, num2):
+    return num1+num2
+
+
+# GUI Code Start Here
+layout = [[sg.Text("Number One"), sg.Input()],
+          [sg.Text("Number Two"), sg.Input()],
+          [sg.Button("Add")]]
+window = sg.Window("Calculator", layout)
 while True:
     event, value = window.read()
-    print(event,value)
-    if event == sg.WIN_CLOSED:
-        break
-    if event == "Change":
-        window['-OUT-'].update(f'you clicked  {event}')
-        print(type(event))
-    
+    print(event, value)
+    match event:
+        case sg.WIN_CLOSED: break
+        case "Add": sg.popup(f"Your Answer Is {add(int(value[0]),int(value[1]))}",
+                             auto_close=True, auto_close_duration=3, title="This is your Answer")  # print(add(int(value[0]),int(value[1])))
 
 
 window.close()
